f722d95a875836cd61e7508db9fe30467568991f
StuartSeupaul <stuartseupaul@gmail.com>
2016-04-12 13:41:13 -0400

    Added validations to product model and added user model with sorcery

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ActiveRecord::Base
+  validates :name, confirmation: true,
+                   length: {minimum:3, maximum: 20},
+                   uniqueness: true
+
+  def formatted_price
+   price_in_dollars = price_in_cents.to_f / 100
+   sprintf("%.2f", price_in_dollars)
+  end
+
+  
+
 end
