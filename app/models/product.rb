72aff694d91e40b1c39242cc4e45159472c9ea53
StuartSeupaul <stuartseupaul@gmail.com>
2016-04-12 14:01:20 -0400

    Added welcome controller, switched to postgres

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,11 +3,8 @@ class Product < ActiveRecord::Base
                    length: {minimum:3, maximum: 20},
                    uniqueness: true
 
-  def formatted_price
-   price_in_dollars = price_in_cents.to_f / 100
-   sprintf("%.2f", price_in_dollars)
-  end
 
-  
+
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412162823) do
+ActiveRecord::Schema.define(version: 20160412174048) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -20,5 +23,15 @@ ActiveRecord::Schema.define(version: 20160412162823) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",            null: false
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
